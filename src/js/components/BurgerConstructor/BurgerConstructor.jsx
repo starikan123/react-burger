60371ef787857062deadc7c30e67ce98b0633116
pavel putilin <starikan123@gmail.com>
2023-05-21 15:23:40 +0300

    changes to burgerconstructor by step 2

--- src/js/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/js/components/BurgerConstructor/BurgerConstructor.jsx
@@ -7,50 +7,57 @@ import {
   Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import burgerConstructorsStyle from "./BurgerConstructor.module.css";
+import ingredientType from "../../utils/types";
+
 import { BurgerContext } from "../../services/BurgerContext";
 
 function BurgerConstructor({ onClick }) {
   const { selectedIngredients, totalPrice } = React.useContext(BurgerContext);
 
-  const renderBurgerElement = (element, type) => {
+  const renderBurgerElement = (ingredient, type) => {
     return (
       <div className="ml-8 pl-4 pr-4">
         <ConstructorElement
           type={type}
-          isLocked={true}
-          text={`${element.name} (${type === "top" ? "верх" : "низ"})`}
-          price={element.price}
-          thumbnail={element.image_mobile}
+          isLocked={type === "bun"}
+          text={`${ingredient.name} (${type === "top" ? "верх" : "низ"})`}
+          price={ingredient.price}
+          thumbnail={ingredient.image_mobile}
         />
       </div>
     );
   };
 
+  const { bun, other } = selectedIngredients;
+  const bunTop = bun || other.find((ingredient) => ingredient.type === "bun");
+
+  const filteredOtherIngredients = other.filter(
+    (ingredient) => ingredient.type !== "bun"
+  );
+
   return (
     <section className={`${burgerConstructorsStyle.board} pt-25`}>
-      {selectedIngredients.bun &&
-        renderBurgerElement(selectedIngredients.bun, "top")}
+      {bunTop && renderBurgerElement(bunTop, "top")}
       <ul className={`${burgerConstructorsStyle.lists} pl-4 pr-4`}>
-        {selectedIngredients.other.map((item) => (
-          <li className={burgerConstructorsStyle.list} key={item._id}>
+        {filteredOtherIngredients.map((ingredient) => (
+          <li className={burgerConstructorsStyle.list} key={ingredient._id}>
             <DragIcon type="primary" />
             <ConstructorElement
               isLocked={false}
-              text={item.name}
-              price={item.price}
-              thumbnail={item.image_mobile}
+              text={ingredient.name}
+              price={ingredient.price}
+              thumbnail={ingredient.image_mobile}
             />
           </li>
         ))}
       </ul>
-      {selectedIngredients.bun &&
-        renderBurgerElement(selectedIngredients.bun, "bottom")}
+      {bunTop && renderBurgerElement(bunTop, "bottom")}
       <div className={`${burgerConstructorsStyle.price} pt-10 pr-4`}>
         <div className={burgerConstructorsStyle.count}>
           <p className="text text_type_digits-medium">{totalPrice}</p>
           <CurrencyIcon type="primary" />
         </div>
-        <Button onClick={onClick} type="primary" size="large">
+        <Button onClick={onClick} htmlType="button" type="primary" size="large">
           Оформить заказ
         </Button>
       </div>
