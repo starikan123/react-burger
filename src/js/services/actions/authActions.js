084b1f2e8710192debc562c573c89cc0cfee5dd4
pavel putilin <starikan123@gmail.com>
2023-07-02 14:09:59 +0300

    made  month-9/step-5

--- src/js/services/actions/authActions.js
+++ src/js/services/actions/authActions.js
@@ -12,6 +12,12 @@ import {
   RESET_PASSWORD_REQUEST,
   RESET_PASSWORD_SUCCESS,
   RESET_PASSWORD_FAILURE,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_FAILURE,
+  UPDATE_USER_REQUEST,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILURE,
 } from "./actionTypes";
 import { setCookie, deleteCookie, getCookie } from "../../utils/cookieHelpers";
 
@@ -202,5 +208,58 @@ export const logoutUser = () => async (dispatch) => {
     }
   } catch (error) {
     console.error("Failed to logout", error);
+  }
+};
+
+export const getUser = () => async (dispatch, getState) => {
+  dispatch({ type: GET_USER_REQUEST });
+  try {
+    const response = await fetch(
+      "https://norma.nomoreparties.space/api/auth/user",
+      {
+        headers: {
+          authorization: getState().auth.accessToken,
+        },
+      }
+    );
+    const data = await response.json();
+    if (data.success) {
+      dispatch({
+        type: GET_USER_SUCCESS,
+        payload: data.user,
+      });
+    } else {
+      dispatch({ type: GET_USER_FAILURE, payload: data.message });
+    }
+  } catch (error) {
+    dispatch({ type: GET_USER_FAILURE, payload: error.toString() });
+  }
+};
+
+export const updateUser = (userInfo) => async (dispatch, getState) => {
+  dispatch({ type: UPDATE_USER_REQUEST });
+  try {
+    const response = await fetch(
+      "https://norma.nomoreparties.space/api/auth/user",
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: getState().auth.accessToken,
+        },
+        body: JSON.stringify(userInfo),
+      }
+    );
+    const data = await response.json();
+    if (data.success) {
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: data.user,
+      });
+    } else {
+      dispatch({ type: UPDATE_USER_FAILURE, payload: data.message });
+    }
+  } catch (error) {
+    dispatch({ type: UPDATE_USER_FAILURE, payload: error.toString() });
   }
 };
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,8 +3,11 @@ import {
   Input,
   EmailInput,
   PasswordInput,
+  Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, updateUser } from "../js/services/actions/authActions";
 import { NavLink } from "react-router-dom";
 
 const ProfilePage = () => {
@@ -14,9 +17,30 @@ const ProfilePage = () => {
     password: "",
   });
 
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  useEffect(() => {
+    setFormData({ ...user });
+  }, [user]);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSave = (event) => {
+    event.preventDefault();
+    dispatch(updateUser(formData));
+  };
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    setFormData({ ...user });
   };
 
   const setActive = ({ isActive }) =>
@@ -28,46 +52,54 @@ const ProfilePage = () => {
         <ul className={styles.navigation}>
           <li>
             <NavLink to="/profile" className={setActive}>
-              <span className="text text_type_main-medium ">Профиль</span>
+              <span className="text text_type_main-medium">Профиль</span>
             </NavLink>
           </li>
           <li>
             <NavLink to="/profile/orders" className={setActive}>
-              <span className="text text_type_main-medium ">
+              <span className="text text_type_main-medium">
                 История заказов
               </span>
             </NavLink>
           </li>
           <li>
             <NavLink to="/profile/logout" className={setActive}>
-              <span className="text text_type_main-medium ">Выход</span>
+              <span className="text text_type_main-medium">Выход</span>
             </NavLink>
           </li>
         </ul>
         <p className="text text_type_main-default text_color_inactive">
-          В&nbsp;этом разделе вы&nbsp;можете изменить свои персональные данные
+          В этом разделе вы можете изменить свои персональные данные
         </p>
       </div>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSave}>
         <Input
-          type={"text"}
-          placeholder={"Имя"}
+          type="text"
+          placeholder="Имя"
           onChange={handleInputChange}
           value={formData.name}
-          name={"name"}
+          name="name"
         />
         <EmailInput
           onChange={handleInputChange}
           value={formData.email}
-          name={"email"}
+          name="email"
           placeholder="E-mail"
         />
         <PasswordInput
           onChange={handleInputChange}
           value={formData.password}
-          name={"password"}
+          name="password"
           placeholder="Пароль"
         />
+        <div className={styles.buttonContainer}>
+          <Button type="secondary" size="medium" onClick={handleCancel}>
+            Отмена
+          </Button>
+          <Button type="primary" size="medium">
+            Сохранить
+          </Button>
+        </div>
       </form>
     </div>
   );
