ce40d26ae01ea56e5671a1aa806c1e6bc45d23fe
pavel putilin <starikan123@gmail.com>
2023-10-05 23:26:34 +0300

    final project

--- src/index.jsx
+++ src/index.jsx
@@ -7,19 +7,33 @@ import rootReducer from "./js/services/reducers/rootReducer";
 import { HashRouter } from "react-router-dom";
 import App from "./js/components/App/App.jsx";
 import api from "./js/utils/api";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(applyMiddleware(thunk.withExtraArgument(api)))
 );
+
+const persistor = persistStore(store);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <HashRouter>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </HashRouter>
     </Provider>
   </React.StrictMode>
